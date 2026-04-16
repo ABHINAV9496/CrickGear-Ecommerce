0993b3c8122509249d804851a91bf1c04c2ccd72
ABHINAV9496 <abhinavramesh74@gmail.com>
2026-04-16 09:31:51 +0530

    deleted unwanted folders and comments

--- backend/order/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',              views.PlaceOrderView.as_view()),
-    path('my/',           views.MyOrdersView.as_view()),
-    path('<int:pk>/cancel/', views.CancelOrderView.as_view()),
-]
