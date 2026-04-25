0671e3f6c97dd6821d819ede2a73f42ecb1ca51d
ABHINAV9496 <abhinavramesh74@gmail.com>
2026-04-25 09:30:21 +0530

    removed duplicate backend folder

--- backend/cart/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.MyCartView.as_view(), name='my-cart'),
-    path('update/', views.CartUpdateView.as_view(), name='cart-update'),
-    path('clear/', views.CartClearView.as_view(), name='cart-clear'),
-]
